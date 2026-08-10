4964ffcc756bedeaf2066c626575e508d13c4681
Rahul <rathorerahul9050@gmail.com>
2026-08-10 14:48:32 +0530

    first change

--- ArrayJava.java
+++ ArrayJava.java
@@ -1,7 +1,8 @@
 public class ArrayJava{
     public static void main(String[] args){
         int val = 23;
+        int a = 25 ;
         System.out.println(val);
-
+        System.out.println(a);
     }
 }
