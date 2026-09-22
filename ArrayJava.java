e44519324a8d0b098f106581903b033f351fd115
Rahul <rathorerahul9050@gmail.com>
2026-09-23 01:22:49 +0530

    Delete ArrayJava.java

--- ArrayJava.java
+++
@@ -1,8 +0,0 @@
-public class ArrayJava{
-    public static void main(String[] args){
-        int val = 23;
-        int a = 25 ;
-        System.out.println(val);
-        System.out.println(a);
-    }
-}
